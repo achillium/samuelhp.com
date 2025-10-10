37ff0904ab91711a7f1f8051bb406abfd80b2e6f
sam-astro <caffenequery@gmail.com>
2025-10-10 12:54:37 -0400

    switch to using github discussions instead

--- generateblog.py
+++ generateblog.py
@@ -71,26 +71,22 @@ def convert_markdown_with_css(markdown_file, css_file, output_file):
 
                 <br>
 
-                <div id="disqus_thread"></div>
-<script>
-    /**
-    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-    /*
-    var disqus_config = function () {{
-    this.page.url = "samuelhp.com/b/{output_file}";  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = "{output_file}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    }};
-    */
-    (function() {{ // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://samuelhp.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    }})();
+                <script src="https://giscus.app/client.js"
+        data-repo="achillium/samuelhp.com"
+        data-repo-id="R_kgDOP6m6Dw"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOP6m6D84Cwezl"
+        data-mapping="title"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="light"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-            </div>
         </body>
         </html>
         """
